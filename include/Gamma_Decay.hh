b3a322e5b78be303bca165c14b1d63505a30b7db
Daniel Rhodes <daniel.m.rhodes@gmail.com>
2020-08-27 10:42:26 -0400

    Simplify Gamm_Decay class

--- include/Gamma_Decay.hh
+++ include/Gamma_Decay.hh
@@ -1,28 +1,22 @@
 #ifndef Gamma_Decay_h
 #define Gamma_Decay_h 1
 
-//#include "G4GeneralPhaseSpaceDecay.hh"
-#include "G4VDecayChannel.hh"
+#include "G4GeneralPhaseSpaceDecay.hh"
 
-//class Gamma_Decay : public G4GeneralPhaseSpaceDecay {
-class Gamma_Decay : public G4VDecayChannel {
+class Gamma_Decay : public G4GeneralPhaseSpaceDecay {
 
 public:
 
-  Gamma_Decay(const G4ParticleDefinition* Parent, const G4ParticleDefinition* daughter, G4double BR);
-  ~Gamma_Decay();
+  Gamma_Decay(const G4ParticleDefinition* Parent, const G4ParticleDefinition* daughter, G4double BR)
+    : G4GeneralPhaseSpaceDecay(Parent->GetParticleName(),BR,2, daughter->GetParticleName(),"gamma") {
 
-  G4DecayProducts* DecayIt(G4double);
-  static G4double Pmx(G4double e, G4double p1, G4double p2);
-
-protected:
-
-  G4DecayProducts* TwoBodyDecayIt();
+    SetParent(Parent);
+    SetParentMass(Parent->GetPDGMass());
+    
+  }
   
-private:
+  ~Gamma_Decay() {}
 
-  G4double parentmass;
-  const G4double* theDaughterMasses;
   
 };
 
