99dcb527c57f45791b7b073f8bcf4c4982bb1011
Daniel Rhodes <daniel.m.rhodes@gmail.com>
2020-08-27 09:47:55 -0400

    First step to gamma-ray angular distributions

--- include/Gamma_Decay.hh
+++ include/Gamma_Decay.hh
@@ -1,23 +1,28 @@
 #ifndef Gamma_Decay_h
 #define Gamma_Decay_h 1
 
-#include "G4GeneralPhaseSpaceDecay.hh"
+//#include "G4GeneralPhaseSpaceDecay.hh"
+#include "G4VDecayChannel.hh"
 
-class Gamma_Decay : public G4GeneralPhaseSpaceDecay {
+//class Gamma_Decay : public G4GeneralPhaseSpaceDecay {
+class Gamma_Decay : public G4VDecayChannel {
 
 public:
 
-  Gamma_Decay(const G4ParticleDefinition* Parent, const G4ParticleDefinition* daughter, G4double BR)
-    : G4GeneralPhaseSpaceDecay(Parent->GetParticleName(),BR,2,daughter->GetParticleName(),"gamma") {
+  Gamma_Decay(const G4ParticleDefinition* Parent, const G4ParticleDefinition* daughter, G4double BR);
+  ~Gamma_Decay();
 
-    SetParent(Parent);
-    SetParentMass(Parent->GetPDGMass());
- 
-  }
-      
-  ~Gamma_Decay() {}
+  G4DecayProducts* DecayIt(G4double);
+  static G4double Pmx(G4double e, G4double p1, G4double p2);
+
+protected:
+
+  G4DecayProducts* TwoBodyDecayIt();
   
 private:
+
+  G4double parentmass;
+  const G4double* theDaughterMasses;
   
 };
 
