142fd95e53142777a337015793cffcfcd6364deb
Daniel Rhodes <daniel.m.rhodes@gmail.com>
2020-09-22 07:37:01 -0400

    Update Polarized Particle class

--- include/Polarized_Particle.hh
+++ include/Polarized_Particle.hh
@@ -8,22 +8,27 @@ class Polarized_Particle {
 
 public:
 
-  Polarized_Particle(G4ParticleDefinition* def, G4int Z, G4int A, G4int J, G4double ex);
-  ~Polarized_Particle();
+  Polarized_Particle(G4ParticleDefinition* def, G4int Z, G4int A, G4double J, G4double ex) : part(def),
+											     spin(J) {
+    polar = new G4NuclearPolarization(Z,A,ex);
+  }
+    
+  ~Polarized_Particle() {}
 
   G4ParticleDefinition* GetDefinition() const {return part;}
   G4NuclearPolarization* GetNuclearPolarization() const {return polar;}
-  G4int GetSpin() const {return spin;}
+  G4double GetSpin() const {return spin;}
+  G4int TwoJ() const {return (G4int)(2*spin + 0.01);}
   
   std::vector<std::vector<G4complex>>& GetPolarization() const {return polar->GetPolarization();}
   
-  void SetPolarization(std::vector< std::vector<G4complex> >& p) {polar->SetPolarization(p);}
+  void SetPolarization(std::vector< std::vector<G4complex> > p) {polar->SetPolarization(p);}
   void Unpolarize() {polar->Unpolarize();}
   
 private:
 
   G4ParticleDefinition* part;
-  G4int spin;
+  G4double spin;
   G4NuclearPolarization* polar;
 
 };
